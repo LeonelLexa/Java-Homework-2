07d2ef85474c7d872c18b553c4c02510e1af6652
LeonelLexa <nartakhov@icloud.com>
2024-05-30 12:22:00 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Калькулятор миль");
 
         System.out.print("Введите стоимость билета в рублях: ");
-        int price = 19000;
+        int price = 19000; // Условная цена билета
 
         int miles = price / 20;
         System.out.println("За покупку билета на " + price + " рублей начислено " + miles + " миль");
