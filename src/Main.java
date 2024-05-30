932526affbb925610710a7daaca344a200e1f408
LeonelLexa <nartakhov@icloud.com>
2024-05-30 12:07:55 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Калькулятор миль");
+
+        System.out.print("Введите стоимость билета в рублях: ");
+        int price = 19000;
+
+        int miles = price / 20;
+        System.out.println("За покупку билета на " + price + " рублей начислено " + miles + " миль");
     }
 }
